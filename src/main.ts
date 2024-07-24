4a95389224e4e8fbf63d1b0d2513d7f645fec03e
DingDean <me@dingkewz.com>
2024-07-24 04:14:35 +0000

    001-draw-a-rectangle

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+
 async function setupWebGPU() {
   // Get the GPUDevice handle
   const adapter = await navigator.gpu?.requestAdapter({
-    
+
   });
   if (!adapter) {
     throw new Error("No adapter found");
@@ -22,6 +23,72 @@ async function setupWebGPU() {
     format: navigator.gpu.getPreferredCanvasFormat(),
   });
 
+  // Prepare the vertices for a square
+  const vertices = new Float32Array([
+    0.5, 0.5,
+    0.5, 0.0,
+    0.0, 0.0,
+    0.5, 0.5,
+    0.0, 0.0,
+    0.0, 0.5,
+  ]);
+
+  // Create the Vertex Buffer
+  const vertexBuffer = device.createBuffer({
+    label: "vertex buffer",
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+  // Describe the layout of vertex buffer
+  const vertexBufferLayout: GPUVertexBufferLayout = {
+    arrayStride: Float32Array.BYTES_PER_ELEMENT * 2,
+    attributes: [{
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0, // Position, see vertex shader
+    }],
+  };
+  // Copy the vertices to the GPU
+  device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+  // Create the vertex shader
+  const vertexShader = device.createShaderModule({
+    label: "vertex shader",
+    code: `
+      @vertex
+      fn vs_main(@location(0) pos: vec2<f32>) -> @builtin(position) vec4<f32> {
+        var output = vec4<f32>(pos, 0.0, 1.0);
+        return output;
+      }
+
+      @fragment
+      fn fs_main() -> @location(0) vec4<f32> {
+        return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+      }
+    `,
+  })
+
+  // create the pipeline
+  const pipeline = device.createRenderPipeline({
+    label: "pipeline",
+    layout: "auto",
+    vertex: {
+      module: vertexShader,
+      entryPoint: "vs_main",
+      buffers: [vertexBufferLayout],
+    },
+    fragment: {
+      module: vertexShader,
+      entryPoint: "fs_main",
+      targets: [{
+        format: navigator.gpu.getPreferredCanvasFormat(),
+      }],
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+  });
+
   // Setup our canvas
   const encoder = device.createCommandEncoder();
   const renderPass = encoder.beginRenderPass({
@@ -34,7 +101,13 @@ async function setupWebGPU() {
       },
     ],
   });
+
+  // Draw the square
+  renderPass.setPipeline(pipeline);
+  renderPass.setVertexBuffer(0, vertexBuffer);
+  renderPass.draw(vertices.length / 2);
   renderPass.end();
+
   device.queue.submit([encoder.finish()]);
 }
 
